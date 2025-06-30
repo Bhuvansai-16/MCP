14bdd4651cd88fc344decf18615e5293f395940d
Chilamkurthi Bhuvansai <151933789+Bhuvansai-16@users.noreply.github.com>
2025-06-30 14:58:04 +0530

    Reduce Playground Height by Additional 30px

--- src/components/PlaygroundView.tsx
+++ src/components/PlaygroundView.tsx
@@ -342,8 +342,8 @@ export const PlaygroundView: React.FC<PlaygroundViewProps> = ({ isDark, initialM
   return (
     <div className="h-full overflow-hidden">
       <div className="container mx-auto px-6 py-8 h-full flex flex-col">
-        {/* Playground Grid - Reduced height by 20px */}
-        <div className="flex-1 grid grid-cols-1 lg:grid-cols-5 gap-6 min-h-0" style={{ height: 'calc(100vh - 200px)' }}>
+        {/* Playground Grid - Reduced height by additional 30px (total 50px reduction) */}
+        <div className="flex-1 grid grid-cols-1 lg:grid-cols-5 gap-6 min-h-0" style={{ height: 'calc(100vh - 230px)' }}>
           {/* Editor Panel - 60% width (3/5) */}
           <div className="lg:col-span-3 flex flex-col min-h-0">
             {/* Editor Mode Selector */}
@@ -399,8 +399,8 @@ export const PlaygroundView: React.FC<PlaygroundViewProps> = ({ isDark, initialM
               </div>
             </div>
 
-            {/* Editor Content - Scrollable with reduced height */}
-            <div className="flex-1 min-h-0" style={{ height: 'calc(100% - 80px)' }}>
+            {/* Editor Content - Scrollable with additional height reduction */}
+            <div className="flex-1 min-h-0" style={{ height: 'calc(100% - 110px)' }}>
               <AnimatePresence mode="wait">
                 <motion.div
                   key={editorMode}
@@ -416,8 +416,8 @@ export const PlaygroundView: React.FC<PlaygroundViewProps> = ({ isDark, initialM
             </div>
           </div>
 
-          {/* Chat Panel - 40% width (2/5) with reduced height */}
-          <div className="lg:col-span-2 flex flex-col min-h-0" style={{ height: 'calc(100% - 20px)' }}>
+          {/* Chat Panel - 40% width (2/5) with additional height reduction */}
+          <div className="lg:col-span-2 flex flex-col min-h-0" style={{ height: 'calc(100% - 50px)' }}>
             <AgentChat
               isDark={isDark}
               messages={messages}
